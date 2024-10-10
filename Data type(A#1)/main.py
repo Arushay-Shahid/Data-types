6798400971cc169d288b2c40ac563097cd6f5fd1
Arushay-Shahid <ummearushay567@gmail.com>
2024-10-10 21:04:56 +0500

    Add files via upload

---
+++ Data type(A#1)/main.py
@@ -0,0 +1,21 @@
+name ="arushay"
+age=15
+is_student=True
+weight=38.5
+
+print("name:", name)
+print("Data Type of Name is", type(name))
+print("Age", age)
+print("Data Type of age is", type(age))
+print("is-student:", is_student)
+print("Data Type of is-student is",type(is_student))
+print("weight:",weight)
+print("Data Type of weight is",type(weight))
+
+print("\n After Type Casting...")
+age=str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of weight is",type(weight))
